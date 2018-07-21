ffac8fa8020c5c07d39b15c693cff51c7d1cb830
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-07-21 18:29:42 +0430

    RandomSpawning

--- MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawn.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawn.cs
@@ -4,9 +4,9 @@ using System.Collections;
 	public class EnemySpawn : MonoBehaviour {
 		public GameObject[] enemies;
 		Vector2 whereToSpawn;
-		public float spawnRate = 5f;
-		public bool stop;
+		public float spawnrate = 5f;
 		public float posX = 1.7f;
+		float nextspawn = 0.0f;
 		int randEnemy;
 		// Use this for initialization
 		void Start () {
@@ -14,11 +14,13 @@ using System.Collections;
 		
 		// Update is called once per frame
 		void Update () {
-		while (!stop) {
-			randEnemy = Random.Range (0, 6);
+			if(Time.time > nextspawn)
+			{
+			nextspawn=Time.time + spawnrate;
+			randEnemy = Random.Range (0, 2);
 			whereToSpawn = new Vector2 (posX, transform.position.y);
-			Instantiate (enemies[randEnemy], whereToSpawn , gameObject.transform.rotation);			
-		}
+			Instantiate (enemies[randEnemy], whereToSpawn , gameObject.transform.rotation);		
+			}
 		}
 			
 	}
