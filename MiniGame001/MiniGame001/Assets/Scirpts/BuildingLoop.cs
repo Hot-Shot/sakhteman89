e3d26a5c038a8af4941e80506370018078501254
Hot-Shot <34549360+Hot-Shot@users.noreply.github.com>
2018-07-21 15:03:07 +0430

    Loop

--- MiniGame001/MiniGame001/Assets/Scirpts/BuildingLoop.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/BuildingLoop.cs
@@ -1,17 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BuildingLoop : MonoBehaviour {
+public class Scroll : MonoBehaviour {
 	
-	public GameObject block;
-	float timer;
-	float randY;
-	float randX;
-	float k=0;
-	Vector2 whereToSpawn;
-	public float spawnRate = 1f;
-	float nextSpawn = 0.5f;
-	
+	public float Speed = 0.5f;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,12 +11,7 @@ public class BuildingLoop : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > nextSpawn) {
-			nextSpawn = Time.time + spawnRate;
-			randY = Random.Range (-5f, 5f);
-			whereToSpawn = new Vector2 (randX, transform.position.y);
-			Instantiate (block, whereToSpawn, Quaternion.identity);
-		}
-
+		Vector2 offset = new Vector2 (0, Time.time * Speed);
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
 	}
 }
