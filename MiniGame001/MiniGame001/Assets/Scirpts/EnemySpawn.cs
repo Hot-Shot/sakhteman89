ef8f33cde01aab73ff0819f489ead3c3bc3dd5aa
Hot-Shot <34549360+Hot-Shot@users.noreply.github.com>
2018-07-23 12:28:23 +0430

    timer

--- MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawn.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawn.cs
@@ -4,16 +4,28 @@ using System.Collections;
 	public class EnemySpawn : MonoBehaviour {
 		public GameObject[] enemies;
 		Vector2 whereToSpawn;
-		public float spawnrate = 5f;
+		public float myTimer;
+		public float spawnrate = 4f;
 		public float posX = 1.7f;
 		float nextspawn = 0.0f;
 		int randEnemy;
+		
 		// Use this for initialization
 		void Start () {
+			myTimer= myTimer + Time.deltaTime;
 		}
 		
 		// Update is called once per frame
 		void Update () {
+			myTimer= myTimer + Time.deltaTime;
+			if(myTimer>=9.9)
+			{
+				spawnrate = spawnrate - 1.4f;
+			}
+			if(myTimer>9.9)
+			{
+				myTimer=0;
+			}	
 			if(Time.time > nextspawn)
 			{
 				nextspawn=Time.time + spawnrate;
