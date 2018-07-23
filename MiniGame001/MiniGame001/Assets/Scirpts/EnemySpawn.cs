213b260a3271dfe91fa2a20f09032267e21ba41f
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-07-23 15:23:25 +0430

    RandomEnemySpawners

--- MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawn.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawn.cs
@@ -1,42 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
-	
-	public class EnemySpawn : MonoBehaviour {
-		public GameObject[] enemies;
-		Vector2 whereToSpawn;
-		public float myTimer;
-		public float spawnrate = 4f;
-		public float posX = 1.7f;
-		float nextspawn = 0.0f;
-		int randEnemy;
-		
-		// Use this for initialization
-		void Start () {
-			myTimer= myTimer + Time.deltaTime;
-		}
-		
-		// Update is called once per frame
-		void Update () {
-			myTimer= myTimer + Time.deltaTime;
-			if(myTimer>=10)
-			{
-				spawnrate -= 1.5f;
-			}
-			if(myTimer>10)
-			{
-				myTimer=0;
-			}	
-			if (spawnrate <= 1) {
-				spawnrate += 1.5f;
-			}
-			if(Time.time > nextspawn)
-			{
-				nextspawn=Time.time + spawnrate;
-				randEnemy = Random.Range (0, 3);
-				whereToSpawn = new Vector2 (posX, transform.position.y);
-				Instantiate (enemies[randEnemy], whereToSpawn , gameObject.transform.rotation);		
-			}
 
-		}
-			
+public class EnemySpawn : MonoBehaviour {
+	public GameObject[] enemies;
+	Vector2 whereToSpawn;
+	public float myTimer;
+	public float spawnrate = 4f;
+	public float posX = 1.7f;
+	float nextspawn = 0.0f;
+	public int a=0;
+	public float i=0.4f;
+	int randEnemy;
+	// Use this for initialization
+	void Start () {
 	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTimer= myTimer + Time.deltaTime;
+		if(myTimer>=10)
+		{
+			a+=1;
+			spawnrate -= 1.5f;
+			myTimer=0;
+		}
+		if (spawnrate <= 1) {
+			spawnrate += i;
+		}
+		if (i <= 0.01) {
+			i += 0.2f;
+		}
+		if (a >= 2)
+		{
+			
+			i -= 0.1f;
+			a = 0;
+		}
+		
+		if(Time.time > nextspawn)
+		{
+			nextspawn=Time.time + spawnrate - i;
+			randEnemy = Random.Range (0, 3);
+			whereToSpawn = new Vector2 (posX, transform.position.y);
+			Instantiate (enemies[randEnemy], whereToSpawn , gameObject.transform.rotation);		
+		}
+		
+	}
+	
+}
--- MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawner3.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/EnemySpawner3.cs
@@ -8,10 +8,11 @@ public class EnemySpawner3 : MonoBehaviour {
 	public float spawnrate = 4f;
 	public float posX = -1.7f;
 	float nextspawn = 0.0f;
+	public int a=0;
+	public float i=0.4f;
 	int randEnemy;
 	// Use this for initialization
 	void Start () {
-		myTimer= myTimer + Time.deltaTime;
 	}
 	
 	// Update is called once per frame
@@ -19,18 +20,26 @@ public class EnemySpawner3 : MonoBehaviour {
 		myTimer= myTimer + Time.deltaTime;
 		if(myTimer>=10)
 		{
+			a+=1;
 			spawnrate -= 1.5f;
-		}
-		if(myTimer>10)
-		{
 			myTimer=0;
-		}	
-		if (spawnrate <= 1) {
-			spawnrate += 1.5f;
 		}
+		if (spawnrate <= 1) {
+			spawnrate += i;
+		}
+		if (i <= 0.01) {
+			i += 0.2f;
+		}
+		if (a >= 2)
+		{
+
+				i -= 0.1f;
+				a = 0;
+			}
+
 		if(Time.time > nextspawn)
 		{
-			nextspawn=Time.time + spawnrate;
+			nextspawn=Time.time + spawnrate - i;
 			randEnemy = Random.Range (0, 3);
 			whereToSpawn = new Vector2 (posX, transform.position.y);
 			Instantiate (enemies[randEnemy], whereToSpawn , gameObject.transform.rotation);		
