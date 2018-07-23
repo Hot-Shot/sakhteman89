d12479e6da6302e361b32bdf01631422dce1cbe6
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-07-23 16:13:44 +0430

    CameraMovmentWithCSharp

---
+++ MiniGame001/MiniGame001/Assets/Scirpts/CameraMovement2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement2 : MonoBehaviour {
+	public float myTimer = 0.0f;
+	public float i = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myTimer += Time.deltaTime;
+		transform.Translate (0, i, 0);
+		if(myTimer>=10)
+		{
+			i+=0.01f;
+		}
+		if (myTimer > 10) 
+		{
+			myTimer = 0f;
+		}
+	}
+}
