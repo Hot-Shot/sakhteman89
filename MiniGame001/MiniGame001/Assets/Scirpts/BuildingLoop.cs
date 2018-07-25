82bcda48168c3dd6e64c4c1801b3748af907c2a5
Hot-Shot <34549360+Hot-Shot@users.noreply.github.com>
2018-07-25 14:18:32 +0430

    buildingLoop

--- MiniGame001/MiniGame001/Assets/Scirpts/BuildingLoop.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/BuildingLoop.cs
@@ -21,7 +21,7 @@ public class BuildingLoop : MonoBehaviour {
 	void Update () {
 		if (Time.time > nextSpawn) {
 			nextSpawn = Time.time + spawnRate;
-			randY = Random.Range (-5f, 5f);
+			randY = Random.Range (0f, 0f);
 			whereToSpawn = new Vector2 (randX, transform.position.y);
 			Instantiate (block, whereToSpawn, Quaternion.identity);
 		}
