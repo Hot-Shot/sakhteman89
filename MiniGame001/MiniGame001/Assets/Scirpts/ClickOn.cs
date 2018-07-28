9fe9092cd70d8d3416a62d64feab5b940cabbec4
Hot-Shot <34549360+Hot-Shot@users.noreply.github.com>
2018-07-28 17:30:42 +0430

    MusicButton

---
+++ MiniGame001/MiniGame001/Assets/Scirpts/ClickOn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Button))]
+public class ClickOn : MonoBehaviour {
+	public Button MusicButton;
+	public Sprite SoundOn;
+	public Sprite Mute;
+	private int counter=0;
+
+	// Use this for initialization
+	void Start () {
+		MusicButton = GetComponent<Button>();
+	
+	}
+	public void ChangeButton(){
+		counter++;
+		if (counter % 2 == 0) {
+			MusicButton.image.overrideSprite = SoundOn;
+		} else {
+			MusicButton.image.overrideSprite = Mute;
+		}
+	}
+}
