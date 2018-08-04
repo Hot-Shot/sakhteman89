22cedff3978fb1a11788abd7e37d5dca5a53226d
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-08-04 13:11:49 +0430

    BirdAnimationAdded

---
+++ MiniGame001/MiniGame001/Assets/Scirpts/BirdMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+	public float PosX;
+	public Vector2 PosiX;
+	public GameObject Bird;
+
+	void Update () 
+	{
+		PosX = transform.position.x;
+		if (PosX <= 5f)
+		{
+			transform.Translate (0.02f, 0, 0);
+		}
+		else if(PosX>=5f)
+		{
+			PosiX = new Vector2 (-4.52f, 4.29f);
+			Instantiate (Bird, PosiX, Quaternion.identity);
+			Destroy(gameObject);
+		}
+	
+	}
+}
