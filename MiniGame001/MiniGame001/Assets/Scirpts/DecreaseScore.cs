7b979455ed5b1c72bae071a88bc95e30091c6b35
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-08-06 10:21:28 +0430

    DeathAnimationAdded

--- MiniGame001/MiniGame001/Assets/Scirpts/DecreaseScore.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/DecreaseScore.cs
@@ -3,13 +3,23 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class DecreaseScore : MonoBehaviour {
+
+	public Animator Death;
 	public Text Dec = null;
 	public Text Hit = null;
+	void update(){
+		bool Dead = false;
+	
+	}
+	void start(){
+	}
 	public void OnMouseDown(){
+
+		Death.SetBool ("Active", true);
 		ScoringSystem.score -= 300;
 		Lives.miss -= 1;
 		Dec.text = "Score: " + ScoringSystem.score;
 		Hit.text = "Lives: " + "" + Lives.miss;
-		Destroy (gameObject);
+		GetComponent<BoxCollider2D> ().enabled = false;
 	}
 }
