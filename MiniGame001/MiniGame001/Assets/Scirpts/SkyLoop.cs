a869294fcbf540216ed81c90e85d3d29a0f9d24a
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-08-06 12:39:34 +0430

    MainMenuSkyBox

---
+++ MiniGame001/MiniGame001/Assets/Scirpts/SkyLoop.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyLoop : MonoBehaviour {
+	public float speed=0.1f;
+	public float myTimer = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			myTimer = myTimer + Time.deltaTime;
+				speed += 0.000001f;
+				myTimer = 0;
+		Vector2 offset = new Vector2 (Time.time * speed,0 );
+				GetComponent<Renderer> ().material.mainTextureOffset = offset;
+			}
+	
+}
