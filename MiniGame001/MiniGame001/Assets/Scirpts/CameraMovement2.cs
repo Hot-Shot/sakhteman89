6bd17c65f100954c5aa1cb1ff1c4ee6575c1aabc
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-08-11 15:20:36 +0430

    FinalPush

--- MiniGame001/MiniGame001/Assets/Scirpts/CameraMovement2.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/CameraMovement2.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class CameraMovement2 : MonoBehaviour {
+	public GameObject H1;
+	public GameObject H2;
+	public GameObject H3;
 	public float myTimer = 0.0f;
 	public float i = 0.01f;
 
@@ -12,6 +15,17 @@ public class CameraMovement2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Lives.miss == 2) 
+		{
+			H1.SetActive(true);
+		}
+		if (Lives.miss == 1)
+		{
+			H2.SetActive(true);
+		}
+		if (Lives.miss == 0) {
+			H3.SetActive (true);
+		}
 		if (PauseMenu.GameIsPaused == false && GameManager.gameHasEnded == false) {
 			myTimer += Time.deltaTime;
 			transform.Translate (0, i, 0);
--- MiniGame001/MiniGame001/Assets/Scirpts/DecreaseScore.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/DecreaseScore.cs
@@ -3,10 +3,23 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class DecreaseScore : MonoBehaviour {
-
+	public GameObject H1;
+	public GameObject H2;
+	public GameObject H3;
 	public Animator Death;
 	public Text Hit = null;
 	void update(){
+		if (Lives.miss == 2) 
+		{
+			H1.SetActive(true);
+		}
+		if (Lives.miss == 1)
+		{
+			H2.SetActive(true);
+		}
+		if (Lives.miss == 0) {
+			H3.SetActive (true);
+		}
 	
 	}
 	void start(){
