3a70190af19ecd5b8ad142ddb738479728a2aca3
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-08-18 09:53:51 +0430

    BugFixes

--- MiniGame001/MiniGame001/Assets/Scirpts/CameraMovement2.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/CameraMovement2.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class CameraMovement2 : MonoBehaviour {
+	public GameObject Hearth1;
+	public GameObject Hearth2;
+	public GameObject Hearth3;
 	public float speed;
 	Vector3 lastPos;
 	// Use this for initialization
@@ -15,6 +18,18 @@ public class CameraMovement2 : MonoBehaviour {
 			return;
 		transform.Translate (Vector2.up * Time.deltaTime * speed * (Mathf.Sqrt( transform.position.y / 100)+1));
 		Vector3 velocity = (this.transform.position - lastPos);
+		if (Lives.miss == 2) {
+			Hearth1.SetActive (true);
+		}
+		if (Lives.miss == 1)
+		{
+			Hearth2.SetActive(true);
+		}
+		if (Lives.miss == 0)
+		{
+			Hearth3.SetActive(true);
+		}
+
 
 	}
 }
