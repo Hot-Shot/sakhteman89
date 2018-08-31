e006526a94f5ae5a192dee8746b634bb497b1c11
Abbaskhadem <abbas.khadem.alhossini0121@gmail.com>
2018-09-01 03:31:59 +0430

    Sounds Added

--- MiniGame001/MiniGame001/Assets/Scirpts/Lives.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/Lives.cs
@@ -3,13 +3,17 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Lives : MonoBehaviour {
+	public AudioClip Wrong;
+	public AudioSource Wrong1;
 	public static int miss=3;
 	public Text Lifes = null;
 	void start() {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		Wrong1.clip = Wrong;
 		if (miss <= 0) 
 		{
 			FindObjectOfType<GameManager>().EndGame();
@@ -19,13 +23,14 @@ public class Lives : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.gameObject.tag == "Enemy") {
-
+			Wrong1.Play();
 			miss-=1;
 			Lifes.text = "Lives: "+""+ miss;
 			Destroy (col.gameObject);
 		} 
 		else if (col.gameObject.tag == "Enemy2")
 		{
+				Wrong1.Play();
 			miss-=1;
 			Lifes.text = "Lives: "+""+ miss;
 			Destroy (col.gameObject);
--- MiniGame001/MiniGame001/Assets/Scirpts/ScoringSystem.cs
+++ MiniGame001/MiniGame001/Assets/Scirpts/ScoringSystem.cs
@@ -3,16 +3,20 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ScoringSystem : MonoBehaviour {
+	public AudioClip Punch;
+	public AudioSource Punch1;
 	public Animator Death;
 	public static int score=0;
 	public Text scores = null;
 	public Text HighScore = null;
 	void Start(){
+		Punch1.clip = Punch;
 		HighScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
 	}
 	public void OnMouseDown() {
 		if (PauseMenu.GameIsPaused||GameManager.gameHasEnded)
 			return;
+		Punch1.Play ();
 		Death.SetBool ("Active", true);
 		score += 100;
 		scores.text =""+ score;
